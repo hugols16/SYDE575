af47ad6069a2dd035bb629e2cc11d6f73c3e55a7
Hugo Louis Seize <hugo.louis.16@gmail.com>
2017-09-20 14:32:51 -0400

    Changed uint8 to double

--- lab1/syde575_lab1.m
+++ lab1/syde575_lab1.m
@@ -12,43 +12,43 @@ img_b_small = imresize(img_a_camera, 0.25, 'bilinear');
  
 % Nearest Neighbor
 img_b_near = imresize(img_b_small, 4, 'nearest');
-error_near = psnr(img_a_camera, img_b_near);
+error_near_cameraman = psnr(img_a_camera, img_b_near);
  
 % Bilinear Interpolation
 img_b_bil = imresize(img_b_small, 4, 'bilinear');
-error_bil = psnr(img_a_camera, img_b_bil);
+error_bil_cameraman = psnr(img_a_camera, img_b_bil);
  
 % Cubic Interpolation
 img_b_cubic = imresize(img_b_small, 4, 'cubic');
-error_cubic = psnr(img_a_camera, img_b_cubic);
+error_cubic_cameraman = psnr(img_a_camera, img_b_cubic);
  
 %% Lena PSNR
-img_a_lena = imread('images/lena.tif');
+img_a_lena = imread('images/lena.tiff');
 img_a_lena = rgb2gray(img_a_lena);
 % Check that the size of the image is a multiple of 4
 rem_x = mod(size(img_a_lena, 1), 4);
 rem_y = mod(size(img_a_lena, 2), 4);
-img_a_camera = img_a_lena(1:size(img_a_lena,1) - rem_x, 1:size(img_a_lena, 2) - rem_y);
+img_a_lena = img_a_lena(1:size(img_a_lena,1) - rem_x, 1:size(img_a_lena, 2) - rem_y);
       
 img_b_small = imresize(img_a_lena, 0.25, 'bilinear');
  
 %% Nearest Neighbor
 img_b_near = imresize(img_b_small, 4, 'nearest');
-error_near = psnr(img_a_lena, img_b_near);
+error_near_lena = psnr(img_a_lena, img_b_near);
  
 %% Bilinear Interpolation
 img_b_bil = imresize(img_b_small, 4, 'bilinear');
-error_bil = psnr(img_a_lena, img_b_bil);
+error_bil_lena = psnr(img_a_lena, img_b_bil);
  
 %% Cubic Interpolation
 img_b_cubic = imresize(img_b_small, 4, 'cubic');
-error_cubic = psnr(img_a_lena, img_b_cubic);
+error_cubic_lena = psnr(img_a_lena, img_b_cubic);
  
  
 %% Lena Convolution
 lena = imread('images/lena.tiff');
 lena = rgb2gray(lena);
-lena = double(lena).*(1/255);
+lena = double(lena).*(1.0/255);
  
 % Convolution Function
 h1 = (1/6)*ones(1,6);
