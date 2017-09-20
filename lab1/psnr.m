3f067c2c1c24a057ad89d98db96970bed4e38999
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-09-20 13:51:39 -0400

    Folder restructure"

---
+++ lab1/psnr.m
@@ -0,0 +1,4 @@
+function PSNR_out = psnr(img_f, img_g)
+max_f = 255.0; % 8-bit image
+mse = (1.0 / (size(img_f,1) * size(img_f,2))) * sum(sum((img_f - img_g).^2));
+PSNR_out = 10*log10(double(max_f^2/mse));
--- lab1/syde575_lab1.m
+++ lab1/syde575_lab1.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 
-img_a = imread('~/Downloads/tire.tif');
+img_a = imread('images/tire.tif');
 % Check that the size of the image is a multiple of 4
 rem_x = mod(size(img_a, 1), 4);
 rem_y = mod(size(img_a, 2), 4);
@@ -20,3 +20,14 @@ error_bil = psnr(img_a, img_b_bil);
 %% Cubic Interpolation
 img_b_cubic = imresize(img_b_small, 4, 'cubic');
 error_cubic = psnr(img_a, img_b_cubic);
+
+%% Discrete Convolution for Image Processing
+
+lena = imread('images/lena.tiff');
+
+lena = grb2gray(lena);
+intensity = double(lena) ./ 255.0;
+
+h1 = (1/6)*ones(1,6);
+h2 = h1';
+h3 = [-1 1];
