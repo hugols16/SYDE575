f57e64d14ebca29bd40e372aa62857b5e7bbf3c6
Hugo Louis Seize <hugo.louis.16@gmail.com>
2017-10-07 12:14:28 -0400

    Lab 2 code

--- syde575_lab0.m
+++
@@ -1,8 +0,0 @@
-close all;
-clear all;
-
-img_a = imread('~/Downloads/cameraman.tiff');
-img_b_inter = imresize(img_a, 0.25, 'bilinear');
-img_b_inter = imresize(img_b, 4, 'bilinear');
-error = psnr(img_a, img_b);
-% imshow(img)
